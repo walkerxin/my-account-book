32703075b1e4befe2dbc94649d747fe7478124cb
walkerxin <1464193658@qq.com>
2020-09-19 10:30:30 +0800

    完成底部导航布局和路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,90 @@
 import React from 'react';
+import {
+  HashRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display:flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+const Nav = styled.nav`
+  >ul {
+    display: flex;
+    >li {
+      width: 33.3333%;
+      text-align: center;
+    }
+  }
+`;
 
 function App() {
   return (
-    <div>App</div>
+    <Wrapper>
+      <Router>
+        <Main>
+          <Switch>
+            <Route path="/tags">
+              <Tags />
+            </Route>
+            <Route path="/money">
+              <Money />
+            </Route>
+            <Route path="/statistics">
+              <Statistics />
+            </Route>
+            <Redirect exact from="/" to="/money" />
+            <Route exact path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Main>
+
+        <div>
+          <Nav>
+            <ul>
+              <li>
+                <Link to="/tags">标签</Link>
+              </li>
+              <li>
+                <Link to="/money">记一笔</Link>
+              </li>
+              <li>
+                <Link to="/statistics">统计</Link>
+              </li>
+            </ul>
+          </Nav>
+        </div>
+      </Router>
+    </Wrapper>
   );
+}
+
+function Statistics() {
+  return <h2>统计页</h2>;
+}
+
+function Tags() {
+  return <h2>标签页</h2>;
+}
+
+function Money() {
+  return <h2>记账页</h2>;
+}
+
+function NoMatch() {
+  return <p>页面不存在，你丫输错了吧！</p>
 }
 
 export default App;
