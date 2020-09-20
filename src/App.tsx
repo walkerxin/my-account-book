205656adafae68f9c449ea3d6799d827acca3bba
walkerxin <1464193658@qq.com>
2020-09-20 22:41:07 +0800

    创建 Layout 组件
    Main + Nav

--- src/App.tsx
+++ src/App.tsx
@@ -5,61 +5,56 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
-import styled from 'styled-components';
-import Nav from 'components/Nav';
-
-const Wrapper = styled.div`
-  min-height: 100vh;
-  display:flex;
-  flex-direction: column;
-`;
-
-const Main = styled.main`
-  flex-grow: 1;
-  overflow: auto;
-`;
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <Wrapper>
-      <Router>
-        <Main>
-          <Switch>
-            <Route path="/tags">
-              <Tags />
-            </Route>
-            <Route path="/money">
-              <Money />
-            </Route>
-            <Route path="/statistics">
-              <Statistics />
-            </Route>
-            <Redirect exact from="/" to="/money" />
-            <Route exact path="*">
-              <NoMatch />
-            </Route>
-          </Switch>
-        </Main>
-        <Nav />
-      </Router>
-    </Wrapper>
+    <Router>
+      <Switch>
+        <Route path="/tags">
+          <Tags/>
+        </Route>
+        <Route path="/money">
+          <Money/>
+        </Route>
+        <Route path="/statistics">
+          <Statistics/>
+        </Route>
+        <Redirect exact from="/" to="/money"/>
+        <Route exact path="*">
+          <NoMatch/>
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+function Tags() {
+  return (
+    <Layout>
+      <h2>标签页</h2>
+    </Layout>
+  );
+}
+
+function Money() {
+  return (
+    <Layout>
+      <h2>记账页</h2>
+    </Layout>
   );
 }
 
 function Statistics() {
-  return <h2>统计页</h2>;
-}
-
-function Tags() {
-  return <h2>标签页</h2>;
-}
-
-function Money() {
-  return <h2>记账页</h2>;
+  return (
+    <Layout>
+      <h2>统计页</h2>
+    </Layout>
+  );
 }
 
 function NoMatch() {
-  return <p>页面不存在，你丫输错了吧！</p>
+  return <p>页面不存在，你丫输错了吧！</p>;
 }
 
 export default App;
---
+++ src/components/Layout.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import Nav from './Nav';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  min-height: 100vh;
+  display:flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex-grow: 1;
+  overflow: auto;
+`;
+
+type Props = {
+	children: object
+}
+
+const Layout = (props : Props) => {
+	console.log('prop ', props.children)
+	return (
+		<Wrapper>
+			<Main>
+				{ props.children }
+			</Main>
+			<Nav/>
+		</Wrapper>
+	)
+}
+
+export default Layout;
