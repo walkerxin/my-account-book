fa79acff904cb720a8482e560dc3daaa369e91e0
walkerxin <1464193658@qq.com>
2020-09-27 22:47:25 +0800

    格式化useTag.tsx文件

--- src/useTags.tsx
+++ src/useTags.tsx
@@ -2,15 +2,15 @@ import {useState} from 'react';
 import {createId} from 'lib/createId';
 
 const defaultTags = [
-	{ id: createId(), name: '衣' },
-	{ id: createId(), name: '食' },
-	{ id: createId(), name: '住' },
-	{ id: createId(), name: '行' }
-]
+	{id: createId(), name: '衣'},
+	{id: createId(), name: '食'},
+	{id: createId(), name: '住'},
+	{id: createId(), name: '行'}
+];
 
 const useTags = () => {
-	const [tags, setTags] = useState<{id: number, name: string}[]>(defaultTags);
-	const findTag = (tagId : number) => tags.filter(tag => tag.id === tagId)[0]
+	const [tags, setTags] = useState<{ id: number, name: string }[]>(defaultTags);
+	const findTag = (tagId: number) => tags.filter(tag => tag.id === tagId)[0];
 	return {tags, setTags, findTag};
 };
 
